fe6cd9b274f25d6b528dd69390cd1a3c3fef2152
yangxindi2017 <694076236@qq.com>
2020-05-21 21:37:16 +0800

    tabbar

--- history/miniprogram/game.js
+++ history/miniprogram/game.js
@@ -1,6 +1,12 @@
-import './js/libs/weapp-adapter'
-import './js/libs/symbol'
+// import './js/libs/weapp-adapter'
+// import './js/libs/symbol'
 
-import Main from './js/main'
+// import Main from './js/main'
 
-new Main()
+// new Main()
+
+const canvas = wx.createCanvas()
+
+const context = canvas.getContext('2d') // 创建一个 2d context
+context.fillStyle = '#1aad19' // 矩形颜色
+context.fillRect(0, 0, 100, 100) 
