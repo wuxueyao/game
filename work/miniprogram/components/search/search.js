2dcb0e78def3cbde7a7524498014d41023fecde1
yangxindi2017 <694076236@qq.com>
2020-05-26 10:31:59 +0800

    ask

--- work/miniprogram/components/search/search.js
+++ work/miniprogram/components/search/search.js
@@ -1,20 +1,12 @@
-// pages/ask/ask.js
+// miniprogram/components/search/search.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    num:1,
-    kind:1,
-    title:'自家的狗子一直呕吐怎么办？！',
-    head_thumb:'xxx',
-    user_name:'用户名',
-    answer:'估计是两个原因:1是你给你喂得太多了,造成了2113累积食,你可以定期给它5261喂一片酵母片,或者消食片,慢慢的它就会好的;2是它4102了一些过敏的食物,下次你观察一下它,它吃过什么东西之后就吐黄水,如是吃了吐黄水,说明它对那1653种食物过敏,以后就不要喂了,换一种食物.',
-    praise:1005,
-    comment:183
-
-
+    inputShowed: false,
+    inputVal: ""
   },
 
   /**
@@ -49,7 +41,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
@@ -72,16 +64,26 @@ Page({
   onShareAppMessage: function () {
 
   },
-  select:function(e){
-    // console.log(e);
+
+  showInput: function () {
     this.setData({
-      num:e.target.dataset.num
-    })
+      inputShowed: true
+    });
   },
-  checked: function (e) {
-    // console.log(e);
+  hideInput: function () {
     this.setData({
-      kind: e.target.dataset.kind
-    })
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
   }
 })
